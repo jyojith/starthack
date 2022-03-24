f7f83c753148c467a41b6027bb9acf0ffaaccc42
Jyojith George Thaliath <jyojith@bigbinary.com>
2022-03-25 00:08:34 +0100

    threshold api set

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
-import lib/regression.py as reg
+import sys
+sys.path.append('./lib')
+import regression as reg
+
 
 
 app = Flask(__name__)
@@ -17,8 +20,9 @@ def home():
 
 @app.route('/api')
 def api():
-    result = reg.regression("data.csv",50,2000)
-    return jsonify({"Message":"This is your flask app with docker"})
+    result = reg.regression("./data.csv",50,2000)
+    print(result)
+    return result
 
 
 if __name__ == "__main__":
--- lib/regression.py
+++ lib/regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from flask import Flask, request, jsonify
 import matplotlib.pyplot as plt
+import json
 from sklearn import linear_model
 
 def regression(filename,threshold,target):
@@ -8,4 +10,10 @@ def regression(filename,threshold,target):
     reg = linear_model.LinearRegression()
     reg.fit(df[['volume']], df.emission)
     result = reg.predict([[target]])
+    if result[0]/target*100 > 30:
+        flag = False
+    else:
+        flag = True
+
+    result = jsonify({"predicted":result[0],"flag":flag})
     return result
